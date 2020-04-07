c31fc80797f184b9e2a3f1f16f24f2e84fa20229
Amitai Burstein <amitai@gizra.com>
2020-04-07 11:59:03 +0300

    Add Robofile

---
+++ RoboFile.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see http://robo.li/
+ */
+class RoboFile extends \Robo\Tasks
+{
+
+  const SITE_URL = 'https://drupal-static-elasticsearch.ddev.site:4443';
+
+  public function staticExport() {
+    $siteUrl = $this::SITE_URL;
+
+    $this->taskExecStack()
+      ->stopOnFail()
+      ->exec("httrack $siteUrl -O ../ -N \"%h%p/%n/index%[page].%t\" --display --robots=0 --footer '' --verbose")
+      ->run();
+
+  }
+}
